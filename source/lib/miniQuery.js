9a2beb8ed513137cee1b985a403f196c2f2cd837
Zohar Liran <zoharliran@gmail.com>
2014-04-08 10:57:15 -0700

    select done

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,24 @@
 /*!
  * minQuery
  */
+miniQuery = function(){
+  
+}
+
+miniQuery.prototype = {
+  select: function(tag){
+    var flag = tag[0];
+    tag = tag.slice(1,tag.length)
+    switch(flag)
+    {
+      case ".":
+        return document.getElementByClassName(tag);
+        break;
+      case "#":
+        return document.getElementById(tag);
+        break;
+    }
+  }
+}
+
+SweetSelector = new miniQuery()
